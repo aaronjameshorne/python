1e21ece4decd2b65abb05bbc6523e1bca94e6367
Aaron Horne <aaronjameshorne57@gmail.com>
2019-11-25 08:03:10 -0500

    Typo for UserData add comma within list.

--- create-ec2.py
+++ create-ec2.py
@@ -3,22 +3,22 @@ import boto3
 import sys
 
 ec2 = boto3.resource('ec2')
+user_data_script = """#!/bin/bash
+sudo apt install htop"""
 
 imageami = input('Enter AMI Image you want to spin up: ')
 string_value = str(imageami)
 ami_count = input('number of instances: ')
 ami_count_int = int(ami_count)
 
-
 def default_ami():
     instances = ec2.create_instances(
-        ImageId='ami-00fc224d9834053d6',
-        MinCount=ami_count_int,
-        MaxCount=ami_count_int,
-        InstanceType='t2.micro',
-        KeyName='webapp'
-        user_data = '''#!/bin/bash
-        echo 'test' > /tmp/hello'''
+       ImageId='ami-0dd655843c87b6930',
+       MinCount=ami_count_int,
+       MaxCount=ami_count_int,
+       InstanceType='t2.micro',
+       KeyName='webapp',
+       UserData=user_data_script
     )
 
 def user_ami():
@@ -27,9 +27,8 @@ def user_ami():
       MinCount=ami_count_int,
       MaxCount=ami_count_int,
       InstanceType='t2.micro',
-      KeyName='webapp'
-      user_data = '''#!/bin/bash
-      echo 'test' > /tmp/hello'''
+      KeyName='webapp',
+      UserData=user_data_script
     )
 
 if string_value == '':
