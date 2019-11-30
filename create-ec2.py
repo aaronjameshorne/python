6339f0215289d9f36b22b89955ce368fcf38763c
Aaron Horne <aaronjameshorne57@gmail.com>
2019-11-30 16:25:47 -0500

    Error handling

--- create-ec2.py
+++ create-ec2.py
@@ -1,34 +1,45 @@
 import os
 import boto3
 import sys
+import traceback
 
 ec2 = boto3.resource('ec2')
 
-user_data_script = """#!/bin/bash sudo apt install htop"""
 imageami = input('Enter AMI Image you want to spin up: ')
 string_value = str(imageami)
 ami_count = input('number of instances: ')
 ami_count_int = int(ami_count)
 
-def default_ami():
-    instances = ec2.create_instances(
-       ImageId='ami-0dd655843c87b6930',
-       MinCount=ami_count_int,
-       MaxCount=ami_count_int,
-       InstanceType='t2.micro',
-       KeyName='webapp',
-       UserData=user_data_script
-    )
 
-def user_ami():
-    instances = ec2.create_instances(
-      ImageId=string_value,
-      MinCount=ami_count_int,
-      MaxCount=ami_count_int,
-      InstanceType='t2.micro',
-      KeyName='webapp',
-      UserData=user_data_script
+def default_ami():
+    try:
+        instances = ec2.create_instances(
+            ImageId='ami-0b2d8d1abb76a53d8',
+            MinCount=ami_count_int,
+            MaxCount=ami_count_int,
+            InstanceType='t2.micro',
+            KeyName='webapp'
     )
+    except:
+        errorFile = open('aws_log.txt','w')
+        errorFile.write(traceback.format_exc())
+        errorFile.close()
+        print('Any errors will be logged to aws_log file')
+        
+def user_ami():
+    try:
+        instances = ec2.create_instances(
+            ImageId=string_value,
+            MinCount=ami_count_int,
+            MaxCount=ami_count_int,
+            InstanceType='t2.micro',
+            KeyName='webapp' 
+    )
+    except:
+        errorFile = open('aws_log.txt','w')
+        errorFile.write(traceback.format_exc())
+        errorFile.close()
+        print('Any errors will be logged to aws_log file')
 
 if string_value == '':
     default_ami()
