9f0778926fa86cd180dbf1402ef1a46c3cd68bf8
Aaron <aaronjameshorne57@gmail.com>
2020-02-14 16:36:09 -0500

    Formatting is horrible. need to fix

--- aws_sdk/web_s3/web_s3.py
+++ aws_sdk/web_s3/web_s3.py
@@ -22,6 +22,29 @@ def list_bucket_objects(bucket):
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
     
+@cli.command('setup-bucket')
+@cli.argument('bucket')
+def setup_bucket(bucket):
+    "Creat and config site"
+    s3_bucket = s3.create_bucket(Bucket='ansible-aws-bucket-site',CreateBucketConfiguration={'LocationConstraint':session.region_name})
+    policy = """
+     {
+      "Version":"2012-10-17",
+      "Statement":[{
+      "Sid":"PublicReadGetObject",
+      "Effect":"Allow",
+      "Principal": "*",
+          "Action":["s3:GetObject"],
+          "Resource":["arn:aws:s3:::%s/*"
+        ]
+      }
+      ]
+     }
+      """
+   policy = policy.strip()
+   pol.put(Policy=policy)
+   ws = new_bucket.Website()
+   ws.put(WebsiteConfiguration={'ErrorDocument':{'Key':'error.html'},'IndexDocument':{'Suffix':'index.html'}})
 
 if __name__ == '__main__':
     cli()
