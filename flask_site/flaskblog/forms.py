8d9c7889687dedf5a2f7e4d588add947c8e04ae3
Aaron <aaronjameshorne57@gmail.com>
2020-04-15 23:40:25 -0400

    adding forget password function to code. 50% completed

--- flask_site/flaskblog/forms.py
+++ flask_site/flaskblog/forms.py
@@ -60,3 +60,22 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = user.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('No account with that email. Please create an account')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
+
+
+            
